e775ae5c14d9248598c5d839c4b4bafa1847cac9
cnyaw <wayncc@gmail.com>
2024-11-05 21:29:56 +0800

    Fixed CalcNewDir of samples/facetarget.

--- samples/facetarget.lua
+++ samples/facetarget.lua
@@ -26,20 +26,10 @@ function CalcNewDir(tx, ty)
   local ox, oy = Good.GetPos(rot_obj_id)
   local a = math.atan2(tx - ox, oy - ty)
   new_dir = a * 180 / math.pi
-  if (0 > new_dir) then
-    new_dir = new_dir + 360
-  end
-  local new_rot_sign = RotSign(last_tx, last_ty, tx - ox, ty - oy)
-  last_tx = tx - ox
-  last_ty = ty - oy
   local dir = Good.GetRot(rot_obj_id)
-  if (180 < math.abs(dir - new_dir)) then
-    if (1 == new_rot_sign) then
-      new_dir = new_dir + 360
-    else
-      new_dir = new_dir - 360
-    end
-  end
+  local delta = new_dir - dir
+  delta = (delta + 180) % 360 - 180     -- Normalize to -180~+180.
+  new_dir = dir + delta
 end
 
 function FaceToTarget(tx, ty)
@@ -66,5 +56,14 @@ FaceTarget.OnStep = function(param)
     local tx, ty = x - param.bw/2, y - param.bh/2
     Good.SetPos(blue_obj_id, tx, ty)
     FaceToTarget(tx, ty)
+  end
+  local dir = Good.GetRot(rot_obj_id)
+  if (dir ~= new_dir) then
+    if (nil ~=param.t) then
+      Good.KillObj(param.t);
+      param.t = nil
+    end
+    local s = string.format("dir=%.2f, new_dir=%.2f", dir, new_dir)
+    param.t = Good.GenTextObj(-1, s, 20)
   end
 end
